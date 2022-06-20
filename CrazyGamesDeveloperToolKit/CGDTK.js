8c8a0fc80d78cea8f1bbbb1f73d367a66b68ef0a
SoftedCo <100989385+SoftedCo@users.noreply.github.com>
2022-06-20 07:38:16 +0600

    Update CGDTK.js

--- CrazyGamesDeveloperToolKit/CGDTK.js
+++ CrazyGamesDeveloperToolKit/CGDTK.js
@@ -87,7 +87,7 @@ class CGDTK {
                 {
                     opcode: 'adState',
                     blockType: Scratch.BlockType.BOOLEAN,
-                    text: 'Is an ad break at the [STATE] state?',
+                    text: 'Ad break [STATE] state true?',
                     arguments: {
                         STATE: {
                             type: Scratch.ArgumentType.STRING,
@@ -224,7 +224,7 @@ class CGDTK {
                     items: ['country code','device']
                 },
                 detections: {
-                    items: ['an adblock','an ad banner','a responsive banner container','an event listener','a page scroll']
+                    items: ['adblock','ad banner','responsive banner container','event listener','page scroll']
                 }
             }
         };
