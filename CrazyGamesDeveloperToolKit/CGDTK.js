474ec3bbb02dd2ebfde09442278459314f16159c
SoftedCo <100989385+SoftedCo@users.noreply.github.com>
2022-06-20 07:23:26 +0600

    Update CGDTK.js

--- CrazyGamesDeveloperToolKit/CGDTK.js
+++ CrazyGamesDeveloperToolKit/CGDTK.js
@@ -13,6 +13,7 @@ class CGDTK {
             blockIconURI: blockIcon,
             menuIconURI: menuIcon,
             docsURI: 'https://softedco.github.io/CrazyGamesDeveloperToolKit/DOCUMENTATION',
+            docsText: 'Open documentation',
             name: 'CrazyGames Developer ToolKit',
             blocks: [
                 {
