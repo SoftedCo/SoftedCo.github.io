ed10a5567c89f73b1d788dbe82dfb14e34723956
SoftedCo <100989385+SoftedCo@users.noreply.github.com>
2022-06-22 21:47:23 +0600

    Update FetchByURL.js

--- FetchByURL/FetchByURL.js
+++ FetchByURL/FetchByURL.js
@@ -1,1 +1,25 @@
-
+class FetchByURL {
+    getInfo() {
+        return {
+            id: 'fetchByURL',
+            name: 'Fetch By URL',
+            blocks: [
+                {
+                    opcode: 'fetchByURL',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Fetch data from [URL]',
+                    arguments: {
+                        URL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'https://softedco.github.io/FetchByURL/FetchTest'
+                        }
+                    }
+                }
+            ]
+        };
+    }
+    fetchByURL(args) {
+        return fetch(args.URL).then(response => response.text())
+    }
+}
+Scratch.extensions.register(new FetchByURL());
