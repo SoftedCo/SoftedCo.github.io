ff23a87a972cc68d97b0dece18b8a689ce296d50
Not actually Ryan <100989385+SoftedCo@users.noreply.github.com>
2022-06-23 11:29:44 +0600

    Update Resolution.js

--- Resolution/Resolution.js
+++ Resolution/Resolution.js
@@ -1,1 +1,27 @@
-
+class resolution {
+    getInfo() {
+        return {
+            id: 'resolution',
+            name: 'Resolution',
+            blocks: [
+                {
+                    opcode: 'width',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Width'
+                },
+                {
+                    opcode: 'height',
+                    blockType: Scratch.BlockType.REPORTER,
+                    text: 'Height'
+                }
+            ]
+        };
+    }
+    width() {
+        return scaffolding.width
+    }
+    height() {
+        return scaffolding.height
+    }
+}
+Scratch.extensions.register(new resolution());
