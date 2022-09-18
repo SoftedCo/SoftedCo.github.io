c3bde4da096dcbdf7df3b7e02e72d8e55dc69dc2
Not actually Ryan <100989385+softedco@users.noreply.github.com>
2022-09-18 15:02:13 +0600

    Update CGDTK.js

--- CrazyGamesDeveloperToolKit/CGDTK.js
+++ CrazyGamesDeveloperToolKit/CGDTK.js
@@ -23,7 +23,7 @@ class CGDTK {
             color3: '#27193f',
             blockIconURI: blockIcon,
             menuIconURI: menuIcon,
-            docsURI: 'https://softedco.github.io/CrazyGamesDeveloperToolKit/DOCUMENTATION',
+            docsURI: 'https://softedco.github.io/CrazyGamesDeveloperToolKit/Documentation',
             name: 'CrazyGames Developer ToolKit',
             blocks: [
                 {
